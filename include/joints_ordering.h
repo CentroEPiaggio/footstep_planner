677a45469938441aa0e4e17c7aca8509914252a6
Mirko <mirko.ferrati@gmail.com>
2014-08-03 17:07:05 +0200

    added single different classes to represent different joint ordering inside chains

---
+++ include/joints_ordering.h
@@ -0,0 +1,37 @@
+#ifndef JOINTSWAISTLEFTFOOT_H
+#define JOINTSWAISTLEFTFOOT_H
+#include <kdl/jntarray.hpp>
+
+class JointsWaistLeftFoot: public KDL::JntArray
+{
+};
+
+class JointsWaistRightFoot: public KDL::JntArray
+{
+};
+
+class JointsLeftFootWaistRightFoot: public KDL::JntArray
+{
+};
+
+class JointsRightFootWaistLeftFoot: public KDL::JntArray
+{
+};
+
+class JointsRightFootWaistLeftFootWaist: public KDL::JntArray
+{
+};
+
+class JointsLeftFootWaistRightFootWaist: public KDL::JntArray
+{
+};
+
+class JointsWaistRightFootWaistLeftFoot: public KDL::JntArray
+{
+};
+
+class JointsWaistLeftFootWaistRightFoot: public KDL::JntArray
+{
+};
+
+#endif // JOINTSWAISTLEFTFOOT_H
