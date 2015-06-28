8755f5daeffe64c9026a1cffd5f61cd1c3690542
MirkoFerrati <mirko.ferrati@gmail.com>
2015-06-28 21:15:22 +0200

    added param manager class

---
+++ include/param_manager.h
@@ -0,0 +1,32 @@
+#ifndef PARAM_MANAGER_H
+#define PARAM_MANAGER_H
+#include <map>
+#include <ros/node_handle.h>
+#include <dynamic_reconfigure/Reconfigure.h>
+
+class param_manager
+{
+public:
+    param_manager();
+    static bool register_param(std::string s, double& d);
+    static bool register_param(std::string s, int& i);
+    static bool register_param(std::string s, std::string& v);
+    static bool update_param(std::string s, double d);
+    static bool update_param(std::string s, int i);
+    static bool update_param(std::string s, std::string v);
+
+
+private:
+    static std::map<std::string,double&> map_double;
+    static std::map<std::string,int&> map_int;
+    static std::map<std::string,std::string&> map_string;
+
+    static ros::NodeHandle* nh;
+    static ros::ServiceServer param_server;
+    bool param_update_callback(dynamic_reconfigure::ReconfigureRequest& req, dynamic_reconfigure::ReconfigureResponse& res);
+};
+
+
+
+
+#endif //PARAM_MANAGER_H
---
+++ src/param_manager.cpp
@@ -0,0 +1,131 @@
+#include "param_manager.h"
+#include <dynamic_reconfigure/Reconfigure.h>
+
+
+bool param_manager::param_update_callback(dynamic_reconfigure::ReconfigureRequest& req, dynamic_reconfigure::ReconfigureResponse& res)
+{
+    if (req.config.strs.at(0).name=="help")
+    {
+        std::string help;
+        help.append(" double params:");
+
+        for (auto name:map_double)
+        {
+            help.append(name.first);
+            help.append(" , ");
+        }
+        help.append("\n int params:");
+        for (auto name:map_int)
+        {
+            help.append(name.first);
+            help.append(" , ");
+        }
+        res.config.strs.clear();
+        res.config.doubles.clear();
+        res.config.ints.clear();
+        dynamic_reconfigure::StrParameter temp;
+        temp.name="result: ";
+        temp.value=help;
+        res.config.strs.push_back(temp);
+        
+        return true;
+    }
+    for(auto param:req.config.doubles)
+    {
+        if (map_double.count(param.name))
+            map_double.at(param.name)=param.value;
+    }
+    for(auto param:req.config.ints)
+    {
+        if (map_int.count(param.name))
+            map_int.at(param.name)=param.value;
+    }
+    for(auto param:req.config.strs)
+    {
+        if (map_string.count(param.name))
+            map_string.at(param.name)=param.value;
+    }
+    res.config.strs.clear();
+    res.config.doubles.clear();
+    res.config.ints.clear();
+    dynamic_reconfigure::StrParameter temp;
+    temp.name="result: ";
+    temp.value="all ok";
+    res.config.strs.push_back(temp);
+    return true;
+}
+
+
+param_manager::param_manager()
+{
+    nh=new ros::NodeHandle();
+    param_server = nh->advertiseService("/footstep_planner/param_update",&param_manager::param_update_callback,this);
+}
+
+
+bool param_manager::register_param(std::string s, std::string& v)
+{
+    if (!map_string.count(s))
+    {
+        map_string.emplace(s,v);
+        return true;
+    }
+    else
+        return false;
+}
+
+bool param_manager::register_param(std::string s, int& i)
+{
+    if (!map_int.count(s))
+    {
+        map_int.emplace(s,i);
+        return true;
+    }
+    else
+        return false;
+}
+
+bool param_manager::register_param(std::string s, double& d)
+{
+    if (!map_double.count(s))
+    {
+        map_double.emplace(s,d);
+        return true;
+    }
+    else
+        return false;
+}
+
+
+bool param_manager::update_param(std::string s, std::string v)
+{
+    if (map_string.count(s))
+    {
+        map_string.at(s)=v;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool param_manager::update_param(std::string s, int i)
+{
+    if (map_int.count(s))
+    {
+        map_int.at(s)=i;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool param_manager::update_param(std::string s, double d)
+{
+    if (map_double.count(s))
+    {
+        map_double.at(s)=d;
+        return true;
+    }
+    else
+        return false;
+}
